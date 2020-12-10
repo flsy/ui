046e1fcefc580d979e981f888666019e2a3bec21
David Tourek <dtourek@centrum.cz>
2020-12-10 15:03:02 +0100

    eslint fix

--- src/modules/DescriptionList/renderValue.tsx
+++ src/modules/DescriptionList/renderValue.tsx
@@ -15,8 +15,10 @@ const Img = styled.img`
 export const isNull = (value: any): value is null => typeof value === 'object' && !value;
 const isObject = (v: any) => v?.constructor === Object && !isNull(v.value) && !v.value;
 const hasProp = (object: object, prop: string) => Object.prototype.hasOwnProperty.call(object, prop);
-const isIData = (value: IDataValue): value is IData => isObject(value);
+const isIData = (value: IDataValue): value is IData => isObject(value) && !isNull(value);
 const isIDataArray = (value: IDataValue): value is IData[] => Array.isArray(value);
+const isBoolean = (value: IDataValue): boolean => !isIDataArray(value) && value.type === 'boolean';
+const isEmptyString = (value: IDataValue): boolean => !isIDataArray(value) && value.value === '';
 export const isILine = (value: IDataValue): value is ILine => !isIDataArray(value) && !isIData(value) && hasProp(value, 'value') && typeof value.value === 'string';
 
 const renderImage = (img: string, name?: string) => {
@@ -24,21 +26,21 @@ const renderImage = (img: string, name?: string) => {
 };
 
 export const renderValue = (key: string, value: IDataValue) => {
-  if (isIData(value)) {
+  if (isNull(value)) {
+    return;
+  }
+
+  if (isEmptyString(value)) {
+    return;
+  }
+
+  if (isIData(value) && !isBoolean(value)) {
     return <DescriptionList data={value} prevKey={key} />;
   }
 
   if (isIDataArray(value)) {
     // eslint-disable-next-line react/no-array-index-key
     return value.map((obj, index) => <DescriptionList key={`${index}-${key}`} data={obj} prevKey={key} index={index} />);
-  }
-
-  if (isNull(value)) {
-    return;
-  }
-
-  if (value.value === '') {
-    return;
   }
 
   switch (value.type) {
